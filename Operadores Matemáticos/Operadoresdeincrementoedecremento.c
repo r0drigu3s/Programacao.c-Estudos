90ef363d88be80e159d08a46685716f471a5a771
André Rodrigues <145060864+r0drigu3s@users.noreply.github.com>
2025-06-13 21:06:06 +0000

    commit

--- Operadores Matemáticos/Operadoresdeincrementoedecremento.c
+++ Operadores Matemáticos/Operadoresdeincrementoedecremento.c
@@ -11,17 +11,31 @@ int main(){
     Pós-Incremento a--
     */
 
-    int numero1 = 1;
+    int numero1 = 1, resultado;
 
     printf("Antes do Incremento: %d\n", numero1);
 
     // numero1 = numero1 +1
     // numero = += 1
-    numero1++;
-    printf("Depois do Incremento: %d\n", numero1);
+    // Pós-Incremetno:
+    // resultado = numero1;
+    //numero1 ++;
+    resultado = numero1++;
+    //printf("Depois do Incremento: %d\n", numero1);
+    printf("Apos Pós-incremento - Númeor1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = ++numero1;
+    printf("Apos Pre-incremento - Númeor1: %d - Resultado: %d\n", numero1, resultado);
 
     // numero1 = numeor1 -1
     // numero1 -= 1
-    numero1--;
-    printf("Depois do Incremento: %d\n",  numero1);
+    //numero1--;
+    //printf("Depois do Incremento: %d\n",  numero1);
+
+    resultado = numero1--;
+    printf("Apos Pós-Decremento - Númeor1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Apos Pré-decremento - Númeor1: %d - Resultado: %d\n", numero1, resultado);
+    
 }

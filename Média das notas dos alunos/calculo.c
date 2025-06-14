57dd99b8d31eec3c130381e9e03d25c26d939b25
André Rodrigues <145060864+r0drigu3s@users.noreply.github.com>
2025-06-14 18:43:30 +0000

    commit

--- Média das notas dos alunos/calculo.c
+++ Média das notas dos alunos/calculo.c
@@ -2,23 +2,6 @@
 
 int main(){
 
-    /*float nota1, nota2, resultado;
-    int soma;
-
-     //Primeira nota do aluno.
-    printf("Primeira Nota: ");
-    scanf("%f", &nota1);
-
-    //Segunda nota do aluno.
-    printf("Segunda Nota: ");
-    scanf("%f", &nota2);
-
-    soma = nota1 + nota2;
-    printf("Soma: %d\n", soma);
-
-    resultado = (float) soma / 2;
-    printf("A nota média do aluno é: %.2f\n", resultado);*/
-
     float nota1, nota2, nota3;
     float media;
 
@@ -35,8 +18,5 @@ int main(){
 
     media = (float)(nota1 + nota2 + nota3) / 3;
     printf("A Média do aluno é %.2f \n", media);
-
-
-
 
 }

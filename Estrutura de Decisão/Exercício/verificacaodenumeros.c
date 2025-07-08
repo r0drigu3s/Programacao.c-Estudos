ce70864780ff466b19d0476b3bf8ee4efff126f7
André Rodrigues <145060864+r0drigu3s@users.noreply.github.com>
2025-07-08 14:03:36 +0000

    commit

--- Estrutura de Decisão/Exercício/verificacaodenumeros.c
+++ Estrutura de Decisão/Exercício/verificacaodenumeros.c
@@ -7,5 +7,19 @@ int main(){
     printf("Digite um número: ");
     scanf("%d", &numero);
 
-    
+    if (numero > 0) {
+        if (numero % 2 == 0) {
+            printf("Número PAR.\n");
+        } else {
+            printf("Número IMPAR.\n");
+        }
+        //printf("Número é Positivo!\n");
+    } else if (numero == 0) {
+        printf("Número é Zero.\n");
+    } else {
+        printf("Número é negativo.\n");
+    }
+
+    return 0;
+
 }

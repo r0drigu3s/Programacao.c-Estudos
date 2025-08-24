4334f05bd7033f61dbb2e8e45cf2cba167cff36b
André Rodrigues <145060864+r0drigu3s@users.noreply.github.com>
2025-08-24 15:24:16 +0000

    commits

---
+++ Estrutura de Repetição/Do-while2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+
+    do {
+
+        printf("Digite um número par para sair do código...");
+        scanf("%d", &numero);
+
+        if (numero % 2 == 0){
+            printf("%d é par!\n", numero);
+        } else {
+            printf("%d é impar!\n", numero);
+        }
+
+    } while (numero % 2 != 0);
+    
+    printf("Você digitou um número par, saindo do programa...\n");
+    
+
+    return 0;
+}
